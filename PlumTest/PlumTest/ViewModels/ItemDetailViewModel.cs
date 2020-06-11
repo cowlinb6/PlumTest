44c40e5e69bac2f9c3e6d5460b856ada374efd60
Ben Cowling <ben.cowling@xarios.com>
2020-06-11 14:24:28 +0100

    Add project files.

---
+++ PlumTest/PlumTest/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using PlumTest.Models;
+
+namespace PlumTest.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
